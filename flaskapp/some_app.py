02f7ba3c9a35574f58810bbc5b3b16e760f42482
PILATb <146848607+PILATb@users.noreply.github.com>
2026-03-04 18:25:38 +0500

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,5 +1,10 @@
-def main():
-    print("OK")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<html><head></head><body>Hello World!</body></html>"
 
 if __name__ == "__main__":
-    main()
+    app.run(host="127.0.0.1", port=5000)
